42d47eb46c77ea7675e79076ccf8da98933cff26
Phebueno <fcbgomes@gmail.com>
2023-05-10 17:49:32 -0300

    feat: funcionalidade simples das rotas get e post para adicionar e recuperar jogos

---
+++ src/controllers/games.controller.js
@@ -0,0 +1,24 @@
+import { db } from "../database/database.connection.js";
+
+export async function getGames(req, res) {
+  try {
+    const games = await db.query(`SELECT * FROM games;`);
+    res.send(games.rows);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+export async function postGame(req, res) {
+  const { name, image, stockTotal, pricePerDay } = req.body;
+  try {
+    const newGame = await db.query(
+      `INSERT INTO games ("name","image","stockTotal", "pricePerDay") 
+        VALUES ($1, $2, $3, $4);`,
+      [name, image, stockTotal, pricePerDay]
+    );
+    res.send("OK");
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { getGames, postGame } from "../controllers/games.controller.js";
 
 const gamesRouter = Router();
+gamesRouter.get("/games", getGames);
+gamesRouter.post("/games", postGame);
 
 export default gamesRouter;
