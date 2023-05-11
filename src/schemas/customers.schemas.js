121de7ec1942d81b87620da6bd59f9a8732428cf
Phebueno <fcbgomes@gmail.com>
2023-05-11 18:23:37 -0300

    fix: correção na validação joi do cpf, aceitar apenas dígitos

--- src/schemas/customers.schemas.js
+++ src/schemas/customers.schemas.js
@@ -1,11 +1,24 @@
 import Joi from "joi";
-import JoiDate from '@joi/date';
+import JoiDate from "@joi/date";
 
 const joi = Joi.extend(JoiDate);
 
 export const customerSchema = joi.object({
-    name: joi.string().required(),
-    phone: joi.string().min(10).max(11).pattern(/^[0-9]+$/).required(),
-    cpf: joi.string().length(11).required(),
-    birthday: joi.date().format('YYYY-MM-DD').required(),
+  name: joi.string().required(),
+  phone: joi
+    .string()
+    .min(10)
+    .max(11)
+    .pattern(/^[0-9]+$/)
+    .messages({
+      "string.pattern.base": `Phone number must have 10 or 11 digits.`,
+    })
+    .required(),
+  cpf: joi
+    .string()
+    .length(11)
+    .pattern(/^[0-9]+$/)
+    .messages({ "string.pattern.base": `CPF number must have 11 digits.` })
+    .required(),
+  birthday: joi.date().format("YYYY-MM-DD").required(),
 });
