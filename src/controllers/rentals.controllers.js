498adb94642c14e7957ca2945bea3533ffa9b7be
Phebueno <fcbgomes@gmail.com>
2023-05-12 19:48:54 -0300

    fix: correção da fórmula da multa

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -65,7 +65,7 @@ export async function finishRental(req, res) {
     SET "returnDate"=$2,
         "delayFee" = CASE
                         WHEN ($2 - "rentDate") > "daysRented"
-                        THEN (($2 - "rentDate")-"daysRented"-1)*"originalPrice"
+                        THEN (($2 - "rentDate")-"daysRented")*"originalPrice"/3
                         ELSE 0
                      END
       WHERE id=$1 AND "returnDate" IS NULL
