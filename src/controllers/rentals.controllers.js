98f8455596ddacc7e4f79faa8e55c1f98c7af319
Phebueno <fcbgomes@gmail.com>
2023-05-11 22:44:11 -0300

    feat: endpoint de post de aluguel de jogos com validações

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -1,4 +1,5 @@
 import { db } from "../database/database.connection.js";
+import dayjs from "dayjs";
 
 export async function getRentals(req, res) {
   try {
@@ -9,8 +10,29 @@ export async function getRentals(req, res) {
 }
 
 export async function postRental(req, res) {
+  const { customerId, gameId, daysRented } = req.body;
+  const rentDate = dayjs().format("YYYY-MM-DD");
   try {
-    res.send(req.body);
+    //só vai atualizar se encontrar os IDS de games e customers nas tabelas
+    const rent = await db.query(`
+    INSERT INTO rentals ("customerId","gameId","rentDate", "daysRented","returnDate","originalPrice","delayFee")
+      SELECT 
+        $1 AS customerId,
+        $2 AS gameId,
+        $3 AS rentDate,
+        $4 AS daysRented,
+        null AS returnDate,
+        "pricePerDay"*$4,
+        null AS delayFee
+          FROM games, customers
+            WHERE games.id=$2 
+              AND customers.id=$1 
+              AND games."stockTotal">(SELECT COUNT(*) FROM rentals WHERE "gameId"=$2);
+      
+    `,[customerId,gameId,rentDate,daysRented]
+    );
+    if(!rent.rowCount) return res.sendStatus(400); //não existe o ID
+    res.sendStatus(201);
   } catch (err) {
     res.status(500).send(err.message);
   }
